262ada2992c10e4d2b0307e333f1eb6362de9c79
shahidx0x <dev.imshahid@gmail.com>
2024-05-26 01:01:45 +0600

    feat(Todo app,design,Layout and components/TodoList): Completed the overall app design
    added some reusable tailwind css class , todolist and update todo list componets are created and combined all componet in the app.tsx file.

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const TodoList = () => {
+  const [edit, setEdit] = useState(false);
+  return (
+    <div className="p-2 border rounded-md">
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <input type="checkbox" />
+          <p className={edit ? `hidden` : `block`}>this is the todo</p>
+          <input
+            type="text"
+            className={edit ? `block edit-input` : `hidden`}
+            defaultValue={`this is the todo`}
+          />
+        </div>
+        <div className="flex gap-2">
+          <button
+            className={edit ? "hidden" : "underline text-blue-500"}
+            onClick={() => setEdit((prev) => !prev)}
+          >
+            Edit
+          </button>
+          <button
+            className={edit ? "underline text-blue-500" : "hidden"}
+            onClick={() => setEdit((prev) => !prev)}
+          >
+            Save
+          </button>
+          <button className="underline text-red-500">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
